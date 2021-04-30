f05544e23a67115957da5807455ded505a7c589f
Ashish291474 <araj04ashish@gmail.com>
2021-04-30 20:23:13 +0530

    pheripherals updated

--- src/peripheral.c
+++ src/peripheral.c
@@ -1,10 +1,15 @@
 #include "peripheral.h"
 #include "activity1.h"
 #include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
 void peripheral(void)
 {
     InitLED(); /*configure led ad button pins*/
     InitADC();
+    InitPWM();
+    InitUART(103);
+    
    
 }
